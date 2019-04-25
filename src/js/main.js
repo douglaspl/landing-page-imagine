bc2a9f4266a362759b4fd69d11550adb4e7dd7a0
douglaspl <douglas.linhares@coreskills.com.br>
2019-04-25 11:58:12 -0300

    Scroll to Form funcionando

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 function toggleNav() {
     document.getElementById('header-nav').classList.toggle('header__nav--is-show');
 }
+
+
+
+//scroll
+
+var element = document.getElementById("form");
+var botao = document.getElementById("button");
+
+botao.addEventListener('click', scrollToForm);
+
+function scrollToForm() {
+    element.scrollIntoView({ behavior: "smooth"});
+};
