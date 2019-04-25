0277b49af21b02ce53b465409a1553bef135f346
douglaspl <douglas.linhares@coreskills.com.br>
2019-04-25 17:17:54 -0300

    Inicio link + modal

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ function toggleNav() {
 
 
 
-//scroll
+//SCROLL INÍCIO
 
 var element = document.getElementById("form");
 var botoes = document.querySelectorAll(".btn");
@@ -26,7 +26,10 @@ btnRegulamento.addEventListener('click', openRules);
 // ----- EXCEÇÃO PARA BOTÃO DO REGULAMENTO FIM
 
 
-// FUNÇÕES
+// SCROLL FIM
+
+
+// ----- FUNÇÕES INÍCIO
 
 function scrollToForm() {
     element.scrollIntoView({ behavior: "smooth"});
@@ -35,3 +38,42 @@ function scrollToForm() {
 function openRules() {
     console.log('Aguardando video de exemplo que virá aqui')
 };
+// ----- FUNÇÕES FIM
+
+
+
+
+// ----- MODAL DE EXEMPLOS INÍCIO
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("tutoriais");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the player iframe
+var player = document.getElementById('player');
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  player.src = "https://player.vimeo.com/video/288253496"
+  modal.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  player.src = "";
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// ----- MODAL DE EXEMPLOS FIM
